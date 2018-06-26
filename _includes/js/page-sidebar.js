c5313fb2acf0d3b0a413b54de73582681fdc8233
Evan Will <evanpeterw@gmail.com>
2018-06-26 15:57:47 -0700

    basic implementation of page sidebar nav

---
+++ _includes/js/page-sidebar.js
@@ -0,0 +1,7 @@
+<script>
+    $(document).ready(function () {
+        $('#sidebarButton').on('click', function () {
+            $('#sidebar').toggleClass('d-block');
+        });
+    });
+</script> 
