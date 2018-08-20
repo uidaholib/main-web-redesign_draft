24e66415ce8f9a3addf3c9840bb398bf66dfe3e4
evanwill <evanpeterw@gmail.com>
2018-08-20 00:46:36 -0700

    simplify featured researchers

--- _includes/js/home.js
+++ _includes/js/home.js
@@ -1,20 +1,7 @@
-{% assign researchers = site.data.vivo-featured %}
+<script src="{{ '/assets/js/ofi.min.js' | relative_url }}"></script>
 <script>
-    var researchers = [{% for researcher in researchers %}"{{researcher.last-name | downcase}}"{% unless forloop.last %},{% endunless %}{% endfor %}];
-    /* Fisher-Yates shuffle https://bost.ocks.org/mike/shuffle/ */
-    function shuffle(array) {
-        var m = array.length, t, i;
-        while (m) {
-            i = Math.floor(Math.random() * m--);
-            t = array[m];
-            array[m] = array[i];
-            array[i] = t;
-        }
-        return array;
-    }
-    shuffle(researchers);
-    //console.log(researchers[0]);
-    $(document).ready(function(){
-        document.getElementById("researcher-" + researchers[0]).style.display = "";
-    });
+    $(function () { objectFitImages() });
+</script>
+<script>
+    $(".researchers").sort(function() { return 0.5 - Math.random() }).first().show();
 </script>
