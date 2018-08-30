fcfd558538c12ee15a89c80ac44ebbc80f350cbd
Evan Will <evanpeterw@gmail.com>
2018-08-30 10:10:27 -0700

    change carousel image to a background, simplifying IE support

--- _includes/js/home.js
+++ _includes/js/home.js
@@ -1,7 +1,3 @@
-<script src="{{ '/assets/js/ofi.min.js' | relative_url }}"></script>
-<script>
-    $(function () { objectFitImages() });
-</script>
 <script>
     $(".researchers").sort(function() { return 0.5 - Math.random() }).first().show();
 </script>
