569f808d7c1893a33d0874431d2822bda6ff52db
evanwill <evanpeterw@gmail.com>
2019-08-14 17:46:02 -0700

    update featured faculty, use id for images

--- _includes/js/home.js
+++ _includes/js/home.js
@@ -14,16 +14,16 @@
 </script>
 
 <script>
-{%- assign researchers = site.data.vivo-featured -%}
+{%- assign researchers = site.data.vivo_featured_researchers | where: 'year','2019' -%}
 {%- assign researcher-fields = 'id;last;first;rank;dept;deptid;pub;publoc;publink;pubtype' | split: ';' -%}
     /* featured researchers */
     var researchers = [
         {% for researcher in researchers %}{ {% for f in researcher-fields %}"{{ f }}":{{ researcher[f] | jsonify }}{% unless forloop.last %},{% endunless %}{% endfor %} }{% unless forloop.last %},{% endunless %}{% endfor %}
     ];
     var featuredResearcher = researchers.sort(function() { return 0.5 - Math.random() }).pop();
-    var featuredCard = '<a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.id + '"> <img class="card-img-top" src="https://www.lib.uidaho.edu/media/vivo-featured/' + featuredResearcher.last.toLowerCase() + '.jpg" alt="' + featuredResearcher.last + 'portrait"></a>' +
-    '<div class="card-body"> <h4 class="card-title"><a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.id + '" style="font-weight:normal">' + featuredResearcher.first + ' ' + featuredResearcher.last + '</a></h4>' +
-    '<p class="card-text">' + featuredResearcher.rank + '<br><a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.deptid + '" style="font-weight:normal;font-size:18px;">' + featuredResearcher.dept + '</a></p>' + 
+    var featuredCard = '<a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.id + '"> <img class="card-img-top" src="https://www.lib.uidaho.edu/media/vivo-featured/' + featuredResearcher.id + '.jpg" alt="' + featuredResearcher.last + 'portrait"></a>' +
+    '<div class="card-body"> <h4 class="card-title"><a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.id + '">' + featuredResearcher.first + ' ' + featuredResearcher.last + '</a></h4>' +
+    '<p class="card-text">' + featuredResearcher.rank + '<br><a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.deptid + '">' + featuredResearcher.dept + '</a></p>' + 
     '<h5>Recent ' + featuredResearcher.pubtype + '</h5>' +
     '<p class="card-text"><a href="' + featuredResearcher.publink + '">' + featuredResearcher.pub + '</a><br><em>' + featuredResearcher.publoc + '</em></p>' +
     '<a href="http://vivo.nkn.uidaho.edu/individual/' + featuredResearcher.id + '" class="btn btn-clearwater ">View VIVO Profile</a></div>';
