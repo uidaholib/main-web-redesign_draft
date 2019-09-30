118f7d64501f0f252d71f2cc60e8ad77c66fe77c
evanwill <evanpeterw@gmail.com>
2019-09-29 23:48:47 -0700

    clean up id bio and style search boxs

--- _includes/js/spec_idahobio.js
+++ _includes/js/spec_idahobio.js
@@ -1,17 +1,11 @@
 <script src="{{ '/assets/datatables/datatables.min.js' | relative_url }}"></script>
 <script>
 (function(){
-    // add text input to each footer heading for search
-    /*
-    $('#item-table tfoot th').each( function () {
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-    } );*/
     // add a text input to each header for search
     $('#item-table thead tr').clone(true).appendTo( '#item-table thead' );
     $('#item-table thead tr:eq(1) th').each( function (i) {
         var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        $(this).html( '<input class="form-control form-control-sm" type="text" placeholder="Filter '+title+'" />' );
  
         $( 'input', this ).on( 'keyup change', function () {
             if ( table.column(i).search() !== this.value ) {
@@ -33,26 +27,13 @@
         // name,bdate,ddate,book,callnumber
         columns: [ { data: '0' }, { data: '1' }, { data: '2' }, { data: '3' }, { data: '4' } ],
         paging: true,
-        orderCellsTop: true,
         lengthMenu: [[ 15, 50, 100, 200], [ 15, 50, 100, 200]],
+        orderCellsTop: true,
         // sort based on the date
         order: [[ 0, "asc" ]],
         // add download features
-        dom: 'i<"row"<"col-md-6"l><"col-md-6"f>>t<"row"<"col-md-6"i><"col-md-6"p>>B',//'Blfrtip', 
+        dom: 'i<"row"<"col-md-6"l><"col-md-6"f>>t<"row"<"col-md-6"i><"col-md-6"p>>B', 
         buttons: [ 'excelHtml5', 'csvHtml5' ]
-    });
-    // Apply column search
-    table.columns().every( function () {
-        var that = this;
- 
-        $( 'input', this.footer() ).on( 'keyup change clear', function () {
-            if ( that.search() !== this.value ) {
-                that
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
-    
+    });    
 })();
 </script>
